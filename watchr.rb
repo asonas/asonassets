ea5a397ca74e8bdd67c196b7680bd02e105591ff
asonas <hzw1258@gmail.com>
2013-03-14 04:11:58 +0900

    watch coffee script

--- watchr.rb
+++ watchr.rb
@@ -1,2 +1,3 @@
 watch('src/index.haml') { system('haml src/index.haml index.html') }
 watch('src/style.scss') { system('sass src/style.scss style.css') }
+watch('src/app.coffee') { system('coffee -c src/app.coffee && mv src/app.js app.js') }
